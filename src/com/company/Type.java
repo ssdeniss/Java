c3733e3be2c7687190386b997c067097b84cb54f
Denis <denis.seremet@snt.md>
2022-10-26 19:47:20 +0300

    'userData'

---
+++ src/com/company/Type.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.lang.String; // for String
+public class Type {
+
+    public static void main(String[] args) {
+        byte numByte = 0; // -128 - 127
+        short numShort = 1; // -32768 - 32767
+        int numInt = 2; // -2^31 - 2^31-1
+        long numLong = 3; // -2^63 - 2^63-1
+        float numFloat = 4.2f; //32-bit
+        double numDouble = 12.13232131231d; // 64-bit
+        boolean bool = true; // true 1 / false 0
+        char symb = 'A'; // one symbol
+        String str = "Lorem ipsum";
+        System.out.print("123123");
+    }
+}
---
+++ src/com/company/UserData.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Scanner; // for data in
+
+public class UserData {
+    public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
+        System.out.println(str.nextLine());
+    }
+}
