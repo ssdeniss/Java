4f3fc521aafa9df8298b31cd90ab291982c75448
Denis <denis.seremet@snt.md>
2022-10-26 19:19:39 +0300

    'some'

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte numByte = 0; // -128 - 127
+        short numShort = 1; // -32768 - 32767
+        int numInt = 2; // -2^31 - 2^31-1
+        long numLong = 3; // -2^63 - 2^63-1
+        float numFloat = 4;
+    }
+}
