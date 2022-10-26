2a60027b0bf87208937b99510dc3029543790d96
Denis <denis.seremet@snt.md>
2022-10-26 22:18:27 +0300

    Multidimensional arrays

---
+++ src/com/company/MultyArray.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.lang.String;
+
+public class MultyArray {
+    public static void main(String[] args) {
+        String[][] array = new String[3][3];
+        array[0][0] = "test test";
+        System.out.print(array[0][0]);
+    }
+}
